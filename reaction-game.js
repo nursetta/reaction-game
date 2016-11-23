5d5b2b600acca71c8dc67e5345bcdb8150c361ba
nursetta <n_ursetta@yahoo.com>
2016-11-23 16:35:37 -0700

    added randomization of time, shape, and color of shapes

--- reaction-game.js
+++ reaction-game.js
@@ -1,13 +1,33 @@
 var start = new Date().getTime();
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 function makeShapeAppear() {
+	var top = Math.random() * 500;
+	var left = Math.random() * 500;
+	var width = Math.random() * 300;
+	document.getElementById("shape").style.top = top + "px";
+	document.getElementById("shape").style.left = left + "px";
+	document.getElementById("shape").style.width = width + "px";
+	document.getElementById("shape").style.height = top + "px";
 	document.getElementById("shape").style.display = "block";
+	document.getElementById("shape").style.backgroundColor = getRandomColor();
+	
 		start = new Date().getTime();
 
 }
 
-setTimeout(makeShapeAppear, Math.random() * 2000);
+function appearAfterDelay() {
+	setTimeout(makeShapeAppear, Math.random() * 3000);
+}
+appearAfterDelay();
 
 document.getElementById("shape").onclick = function() {
 	document.getElementById("shape").style.display = "none";
@@ -16,5 +36,5 @@ document.getElementById("shape").onclick = function() {
 	
 	document.getElementById("timeTaken").innerHTML = timeTaken + " seconds";
 
-	makeShapeAppear();
+	appearAfterDelay();
 };
