2526edc89ada56f2ec5c881798011755eccc014a
nursetta <n_ursetta@yahoo.com>
2016-11-23 01:25:17 -0700

    make square appear at random times

--- reaction-game.js
+++ reaction-game.js
@@ -1,5 +1,20 @@
+var start = new Date().getTime();
 
+
+function makeShapeAppear() {
+	document.getElementById("shape").style.display = "block";
+		start = new Date().getTime();
+
+}
+
+setTimeout(makeShapeAppear, Math.random() * 2000);
 
 document.getElementById("shape").onclick = function() {
 	document.getElementById("shape").style.display = "none";
+	var end = new Date().getTime();
+	var timeTaken = (end - start) / 1000;
+	
+	document.getElementById("timeTaken").innerHTML = timeTaken + " seconds";
+
+	makeShapeAppear();
 };
