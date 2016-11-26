82579e0c157b99f07d37a2cb04d30aa47c1d52b6
nursetta <n_ursetta@yahoo.com>
2016-11-25 17:25:23 -0700

    added triangle shape

--- reaction-game.js
+++ reaction-game.js
@@ -12,19 +12,30 @@ function getRandomColor() {
 function makeShapeAppear() {
 	var top = Math.random() * 500;
 	var left = Math.random() * 500;
-	var width = (Math.random() * 100) + 100;
+	// var width = (Math.random() * 100) + 100;
 
-	if (Math.random() > 0.5) {
-		document.getElementById("shape").style.borderRadius = "50%";
-	} else {
-		document.getElementById("shape").style.borderRadius = "0";
-	}
+	var shape= document.getElementById("shape");
+  if (Math.random()<=0.3){
+    shape.style.borderLeft= shape.style.borderRight= shape.style.borderBottom= shape.style.borderTop= '75px solid';
+    shape.style.borderRadius="50%";
+  }
+  else if (Math.random()<=0.6){
+    shape.style.borderLeft= shape.style.borderRight= shape.style.borderBottom= shape.style.borderTop= '75px solid';
+    shape.style.borderRadius="0";
+  }
+  else {
+    shape.style.borderLeft= shape.style.borderRight= '100px solid transparent';
+    shape.style.borderBottom= '125px solid';
+    shape.style.borderTop= '0';
+    shape.style.borderRadius="0";
+  }
+
 	document.getElementById("shape").style.top = top + "px";
 	document.getElementById("shape").style.left = left + "px";
-	document.getElementById("shape").style.width = width + "px";
-	document.getElementById("shape").style.height = width + "px";
+	// document.getElementById("shape").style.width = width + "px";
+	// document.getElementById("shape").style.height = width + "px";
 	document.getElementById("shape").style.display = "block";
-	document.getElementById("shape").style.backgroundColor = getRandomColor();
+	document.getElementById("shape").style.color = getRandomColor();
 	
 		start = new Date().getTime();
 
